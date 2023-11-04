5ee7dc3dc235478ea8f905d7fbf0b0c63e56fa4d
apikammar <apikammar07@gmail.com>
2023-11-04 23:38:08 +0800

    componentfirstbatch

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+    <footer class="bg-dark text-white text-center py-3 fixed-bottom">
+        <div class="container">
+            <p class="h5">&copy; {{ date('Y') }} By Afiq Ammar 2023. All rights reserved.</p>
+        </div>
+    </footer>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('components/master')
+
+@section('content')
+    <p class="h3">fffff</p>
+@endsection
